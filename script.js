d429fd51f0423925f126134a3ab6f0bcce254c1f
Elijah Hynson <30732234+Hynson@users.noreply.github.com>
2021-06-21 20:18:15 +0000

    Completed steps up to 7.3

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-// Author:
+// Author: Elijah Hynson
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textp;
 
 // Global ML Variables
-//let mobilenet;
+let mobilenet;
+let img;
 
 function setup() {
-
+  canvasDiv = createDiv();
+  canvas = createCanvas(640,480);
+  canvas.parent(canvasDiv);
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait...");
+  textP.parent(textDiv);
+  img = loadImage("images/guinea-pig.jpg", imageLoaded);
 }
 
 function draw() {
@@ -15,13 +25,22 @@ function draw() {
 }
 
 function imageLoaded() {
-
+  image(img, 0, 0, width, height);
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
 }
 
 function modelReady() {
-
+  mobilenet.classify(canvas, gotResults);
 }
 
 function gotResults(error, results) {
+  if(error) {
 
+    console.error(error);
+
+  } else {
+
+    console.log(results);
+
+  }
 }
